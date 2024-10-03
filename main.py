0b975e3c7d5dbebd6e8cabc79eaeecd3fc4b9af1
Shehryar1021 <kiranimtiaz1985@gmail.com>
2024-10-03 16:23:05 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Take marks as input from user
+print("ENTER MARKS OBTAINED IN 3 SUBJECTS:")
+math=int(input ("math :" ))
+urdu=int(input ("urdu :" ))
+english=int(input ("english :" ))
+ 
+ #LETS CALCULATE THE PERCENTAGE OF MARKS
+ sum = ("math+urdu+english")
+print("sum of math,english and urdu")
+
+perc = (sum/400)*100
+
+print(end="PERCENTAGE MARK = ")
+print(perc)
